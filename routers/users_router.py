6ffd329dd4d7ce4e35b4a6b7866537ee968e43d3
Michael Mark <56435670+MichaelMIL@users.noreply.github.com>
2022-11-02 11:31:05 +0200

    added set users location

--- routers/users_router.py
+++ routers/users_router.py
@@ -1,38 +1,84 @@
-
-from fastapi import APIRouter, Body, Request, Depends
+from typing import Union
+from fastapi import APIRouter, Body, HTTPException, Request, Depends
 from fastapi import APIRouter
 from actions.auth_actions import verify_jwt_token
+from models.auth_model import Token
 from models.user_model import CreateUser, User
-from actions.user_actions import _create_user,_delete_user,_get_user,_get_user_by_phone,_update_user
+from actions.user_actions import (
+    _create_user,
+    _delete_user,
+    _get_user,
+    _get_user_by_phone,
+    _update_user,
+)
 
 user_router = APIRouter()
 
-@user_router.post('/', response_model=User)
-async def create_user(request: Request,user: CreateUser = Body(...),token : str =  Depends(verify_jwt_token)):
-    user = _create_user(request=request,user=user)
+
+@user_router.post("/", response_model=User)
+async def create_user(
+    request: Request,
+    user: CreateUser = Body(...),
+    token: Token = Depends(verify_jwt_token),
+):
+    user = _create_user(request=request, user=user)
     return user
 
-@user_router.get('/{UserId}', response_model=User)
-async def get_user(request: Request,UserId:str,token : str =  Depends(verify_jwt_token)):
-    user = _get_user(request=request,user_id=UserId)
+
+@user_router.get("/{UserId}", response_model=User)
+async def get_user(
+    request: Request, UserId: str, token: Token = Depends(verify_jwt_token)
+):
+    user = _get_user(request=request, user_id=UserId)
     return user
 
-@user_router.get('/phone/{UserPhoneNumber}', response_model=User)
-async def get_user_by_phone(request: Request,UserPhoneNumber:str,token : str =  Depends(verify_jwt_token)):
+
+@user_router.get("/phone/{UserPhoneNumber}", response_model=User)
+async def get_user_by_phone(
+    request: Request, UserPhoneNumber: str, token: Token = Depends(verify_jwt_token)
+):
     user = _get_user_by_phone(request=request, user_phone=UserPhoneNumber)
     return user
 
-@user_router.delete('/{UserId}', response_model=None)
-async def delete_user(request: Request,UserId:str,token : str =  Depends(verify_jwt_token)):
+
+@user_router.delete("/{UserId}", response_model=None)
+async def delete_user(
+    request: Request, UserId: str, token: Token = Depends(verify_jwt_token)
+):
     _delete_user(request=request, user_id=UserId)
 
-@user_router.put('/{UserId}', response_model=dict)
-async def update_user(request: Request,UserId:str, user:dict = Body(...),token : str =  Depends(verify_jwt_token)):
-    updated_user = _update_user(request=request, user_id=UserId, user=user)
+
+@user_router.put("/{UserId}", response_model=dict)
+async def update_user(
+    request: Request,
+    UserId: str,
+    user: dict = Body(...),
+    token: Token = Depends(verify_jwt_token),
+):
+    if UserId != token["id"]:
+        raise HTTPException(400, "User can not change othe user")
+    updated_user = _update_user(request=request, user_id=token["id"], user=user)
     return updated_user
 
 
-@user_router.get('/in-range', response_model=list[User])
-async def get_users_in_range(request: Request,token : str =  Depends(verify_jwt_token)):
-    print('Need To BE DONE!')
+@user_router.put("/set-location/{Lat}/{lon}", response_model=dict)
+async def update_user_location(
+    request: Request,
+    Lat: Union[str, int, float],
+    Lon: Union[str, int, float],
+    token: Token = Depends(verify_jwt_token),
+):
+    return await update_user(
+        request=request,
+        UserId=token["id"],
+        user={"user_location": {"lat": Lat, "lon": Lon}},
+        token=token,
+    )
+
+
+@user_router.get("/in-range", response_model=list[User])
+async def get_users_in_range(
+    request: Request, token: Token = Depends(verify_jwt_token)
+):
+    print("Need To BE DONE!")
     return []
